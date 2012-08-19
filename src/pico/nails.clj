1d8fa68abdb11e632d054e2c4ae6669698d550ac
Matti Hänninen <mha@iki.fi>
2012-08-19 16:55:04 +0300

    Update docs

--- src/pico/nails.clj
+++ src/pico/nails.clj
@@ -32,19 +32,17 @@
     :doc
   "Adds a nail `f` to NailGun server.
 
-  The argument `id` must be a keyword that serves dual purpose. On the
-  one hand `id` determines the alias which is set to `(name id)`. On the
-  other hand `id` is used to as a key that can be used for example when
-  removing the nail from the server.
+  The argument `id` must be a keyword that serves dual purpose. On the one
+  hand `id` determines the alias which is set to `(name id)`. On the other
+  hand `id` is used to as a key that can be used for example when removing the
+  nail from the server.
 
-  The argument `f` must be function that takes one argument, i.e. of the
-  form `(fn [c] ...)`. Here `c` is a map that provides the context of
-  the NailGun call. For example `(:args c)` returns a vector holding the
-  client's command line arguments.
+  The argument `f` must be function that accepts the client's command line
+  arguments as its arguments. This mirrors the behavior of a class main. An
+  useful idiom for collecting command line arguments is `(fn [& args] ...)`.
 
   Optional kwarg `:desc` can be used to provide a short description. This
-  description is displayed when user runs `ng ng-alias` on the command
-  line."}
+  description is displayed when user runs `ng ng-alias` on the command line."}
   add-nail server/add-nail)
 
 (def
